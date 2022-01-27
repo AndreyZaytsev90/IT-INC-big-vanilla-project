433f34f972c2fd220e9e6531f74913c5fda83c89
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-27 14:12:37 +0300

    change file

--- JS-TS/src/05-Map/06-Callbacks.tsx
+++ JS-TS/src/05-Map/06-Callbacks.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, {ChangeEvent, MouseEvent} from 'react'
 
 
 export const User = () => {
 
-    const deleteUser = () => {
-        alert("user should be deleted")
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        //event.currentTarget.name
+        alert(event.currentTarget.name)
     }
 
-    const saveUser = () => {
+   /* const saveUser = () => {
         alert("user should be saved")
+    }*/
+
+    const onNameChanged = () => {
+        console.log("name changed")
+    }
+    const focusLostHandler = () => {
+        console.log("focus lost")
+    }
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log("age changed: " + event.currentTarget.value)
     }
 
 
-    return <div>Andrey
-        <div onClick={deleteUser}>delete</div>
-        <button onClick={saveUser}>save</button>
+    return <div><textarea onChange={onNameChanged} onBlur={focusLostHandler}>Andrey</textarea>
+        <input onChange={onAgeChanged} type={"number"}/>
+        <button name="delete" tabIndex={3} onClick={deleteUser}>delete</button>
     </div>
 
 }
