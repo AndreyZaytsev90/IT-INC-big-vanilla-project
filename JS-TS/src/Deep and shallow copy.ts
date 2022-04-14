11e1c2486373e49a4cab19f235c22c5bb4d9e1da
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-04-14 23:02:55 +0300

    change file

---
+++ JS-TS/src/Deep and shallow copy.ts
@@ -0,0 +1,15 @@
+const a = {
+    name: "It-kamasutra.com",
+    protocol: "https",
+    maxStudentCount: 30,
+    isOnline: true,
+    students: ["Andrey", "Olga", "Petr"],
+    classroom: {
+        teatcher: {
+            name: "Dimuch",
+            age: 34
+        }
+    }
+};
+
+console.log(a)
