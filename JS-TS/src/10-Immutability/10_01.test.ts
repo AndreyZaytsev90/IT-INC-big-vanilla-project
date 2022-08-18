7e1cade1cd6828ec96f28ab6602c163456742c48
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-08-18 11:24:38 +0300

    Change file

--- JS-TS/src/10-Immutability/10_01.test.ts
+++ JS-TS/src/10-Immutability/10_01.test.ts
@@ -1,7 +1,7 @@
-import {makeHairstyle, moveUser, UserType, UserWithLaptopType} from "./10_01";
+import {makeHairstyle, moveUser, upgradeUsersLaptop, upgradeUsersLaptop2, UserType, UserWithLaptopType} from "./10_01";
 
 
-test("r", () => {
+test("reference type test", () => {
     let user: UserType = {
         name: "Andrey",
         hair: 32,
@@ -37,10 +37,53 @@ test("change address", () => {
     expect(user.address).not.toBe(movedUser.address)
     expect(user.laptop).toBe(movedUser.laptop)
     expect(movedUser.address.city).toBe("Aleksin")
-
-
-
 })
 
+test("upgrade laptop to macBook", () => {
+    let user: UserWithLaptopType = {
+        name: "Andrey",
+        hair: 32,
+        address: {
+            city: "Moscow",
+            house: 1
+        },
+        laptop: {
+            title: "Lenovo"
+        }
+    }
 
- // закончил на 37 мин
+    const upgradeUser = upgradeUsersLaptop(user, "macBook")
+
+    expect(user).not.toBe(upgradeUser)
+    expect(user.address).toBe(upgradeUser.address)
+    expect(user.laptop).not.toBe(upgradeUser.laptop)
+    expect(user.laptop.title).toBe("Lenovo")
+    expect(upgradeUser.laptop.title).toBe("macBook")
+})
+
+test("upgrade color and model in Lenovo", () => {
+    let user: UserWithLaptopType = {
+        name: "Andrey",
+        hair: 32,
+        address: {
+            city: "Moscow",
+            house: 1
+        },
+        laptop: {
+            title: "Lenovo",
+            color: "Black",
+            model: "B570e"
+        }
+    }
+
+    const upgradeUserLaptop2 = upgradeUsersLaptop2(user, "Silver", "IdeaPad")
+
+    expect(user).not.toBe(upgradeUserLaptop2)
+    expect(user.address).toBe(upgradeUserLaptop2.address)
+    expect(user.laptop).not.toBe(upgradeUserLaptop2.laptop)
+    expect(user.laptop.title).toBe("Lenovo")
+    expect(user.laptop.color).toBe("Black")
+    expect(user.laptop.model).toBe("B570e")
+    expect(upgradeUserLaptop2.laptop.color).toBe("Silver")
+    expect(upgradeUserLaptop2.laptop.model).toBe("IdeaPad")
+})
--- JS-TS/src/10-Immutability/10_01.ts
+++ JS-TS/src/10-Immutability/10_01.ts
@@ -1,35 +1,52 @@
 export type UserType = {
-    name: string
-    hair: number
-    address: {city: string, house?: number}
+  name: string
+  hair: number
+  address: {
+    city: string,
+    house?: number
+  }
 }
 
 export type LaptopType = {
-    title: string
+  title: string,
+  color?: string,
+  model?: string
 }
 
 export type UserWithLaptopType = UserType & {
-    laptop: LaptopType
+  laptop: LaptopType
 }
 
- export function makeHairstyle (u: UserType, power: number) {
-    const copyUser ={
-        ...u,
-        hair: u.hair / power
-    }
-    //copyUser.hair = u.hair / power
-
-    return copyUser
-
+export function makeHairstyle(u: UserType, power: number) {
+  const copyUser = {
+    ...u,
+    hair: u.hair / power
+  }
+  //copyUser.hair = u.hair / power
+  return copyUser
 }
 
-export function moveUser (u: UserWithLaptopType, title: string) {
-    return {
-        ...u,
-        address: {
-            ...u.address,
-            city: "Aleksin"}
+export function moveUser(u: UserWithLaptopType, city: string) {
+  return {
+    ...u, address: {...u.address, city: city}
+  }
+  /*
+  copyMoveUser.address = {
+  ...copyMoveUser.address,
+   city: "Aleksin"}
+   */
+}
 
-    }
-    /*copyMoveUser.address = { ...copyMoveUser.address, city: "Aleksin"}*/
+export function upgradeUsersLaptop (u: UserWithLaptopType, title: string) {
+  return {
+    ...u, laptop: {...u.laptop, title: title}
+  }
+}
+
+export const upgradeUsersLaptop2 = (u: UserWithLaptopType,
+                                    color: string,
+                                    model: string) => {
+  return {
+    ...u, laptop: { ...u.laptop, color: color, model: model}
+  }
 }
