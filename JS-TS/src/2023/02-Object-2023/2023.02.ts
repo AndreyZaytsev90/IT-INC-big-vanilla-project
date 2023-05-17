2bd6a93571c5f2a988276a5458ee235c9e35e9e3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-05-17 22:31:55 +0300

    Updated file

--- JS-TS/src/2023/02-Object-2023/2023.02.ts
+++ JS-TS/src/2023/02-Object-2023/2023.02.ts
@@ -1,6 +1,36 @@
 const student = {
-    "name" : "Andrey",
+    "name": "Andrey",
     "age": 32,
-    "isActive": false
+    "isActive": false,
+    "Моя Фамилия": "Zaytsev",
+    "address": {
+        streetTitle: 'Arbat',
+        city: {
+            title: 'Moscow',
+            countryTitle: 'Russia'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'CSS'
+        },
+        {
+            id: 2,
+            title: 'HTML'
+        },
+        {
+            id: 3,
+            title: 'JS'
+        },
+        {
+            id: 4,
+            title: 'React'
+        },
+    ]
 }
 console.log(student.name)
+console.log(student.age)
+console.log(student["Моя Фамилия"])
+console.log(student.address.city.title)
+console.log(student.technologies[3].title)
