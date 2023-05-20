a88f829cf9d29f68365885ae823406da8189cc6f
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-05-20 18:58:16 +0300

    Updated file

---
+++ JS-TS/src/2023/02-Object-2023/2023.02.test.ts
@@ -0,0 +1,76 @@
+import {CityType} from "./2023.02_02";
+
+
+let city: CityType
+
+beforeEach(() => {
+    city = {
+        title: "Moscow",
+        houses: [
+            {
+                buildedAt: 2013, repaired: false,
+                address: {number: 1, street: {title: "Lipchanskogo"}}
+            },
+            {
+                buildedAt: 1980, repaired: false,
+                address: {number: 8, street: {title: "Sormovskaya"}}
+            },
+            {
+                buildedAt: 1969, repaired: false,
+                address: {number: 4, street: {title: "Uralskaya"}}
+            }
+            ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL", budget: 200000, staffCount: 1000,
+                address: {
+                    street: {
+                        title: "Nedorubova"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION", budget: 500000, staffCount: 1000,
+                address: {
+                    street: {
+                        title: "Dmitrievsky"
+                    }
+                }
+            }],
+        citizensNumber: 12000000
+    }
+})
+
+test("city should contains 3 houses", () => {
+    expect(city.houses.length).toBe(3);
+
+    expect(city.houses[0].buildedAt).toBe(2013);
+    expect(city.houses[0].repaired).toBe(false);
+    expect(city.houses[0].address.number).toBe(1);
+    expect(city.houses[0].address.street.title).toBe("Lipchanskogo");
+
+    expect(city.houses[1].buildedAt).toBe(1980);
+    expect(city.houses[1].repaired).toBe(false);
+    expect(city.houses[1].address.number).toBe(8);
+    expect(city.houses[1].address.street.title).toBe("Sormovskaya");
+
+    expect(city.houses[2].buildedAt).toBe(1969);
+    expect(city.houses[2].repaired).toBe(false);
+    expect(city.houses[2].address.number).toBe(4);
+    expect(city.houses[2].address.street.title).toBe("Uralskaya");
+})
+
+test("city should contains hospital and fire station", () => {
+    expect(city.governmentBuildings.length).toBe(2);
+
+    expect(city.governmentBuildings[0].type).toBe("HOSPITAL")
+    expect(city.governmentBuildings[0].budget).toBe(200000)
+    expect(city.governmentBuildings[0].staffCount).toBe(1000)
+    expect(city.governmentBuildings[0].address.street.title).toBe("Nedorubova")
+
+    expect(city.governmentBuildings[1].type).toBe("FIRE-STATION")
+    expect(city.governmentBuildings[1].budget).toBe(500000)
+    expect(city.governmentBuildings[1].staffCount).toBe(1000)
+    expect(city.governmentBuildings[1].address.street.title).toBe("Dmitrievsky")
+
+})
--- JS-TS/src/2023/02-Object-2023/2023.02.ts
+++ JS-TS/src/2023/02-Object-2023/2023.02.ts
@@ -1,4 +1,45 @@
-const student = {
+type TomatoType = {
+    id: number,
+    address: {
+        country: string,
+        city: string
+    },
+    places: {
+        quantity: number,
+        info: InfoType[]
+    }
+}
+type InfoType = {
+    id: number,
+    busy: boolean
+}
+
+type StudentType = {
+    name: string,
+    age: number,
+    isActive: boolean,
+    "Моя Фамилия": string,
+    address: AddressType,
+    technologies: TechnologiesType[]
+}
+
+type AddressType = {
+    streetTitle: string,
+    city: LocalCityType
+}
+
+type LocalCityType = {
+    title: string,
+    countryTitle: string
+}
+
+type TechnologiesType = {
+    id: number,
+    title: string
+}
+
+
+const student: StudentType = {
     "name": "Andrey",
     "age": 32,
     "isActive": false,
@@ -29,8 +70,26 @@ const student = {
         },
     ]
 }
+
+const tomato: TomatoType = {
+    id: 1,
+    address: {
+        country: 'Russia',
+        city: "Aleksin"
+    },
+    places: {
+        quantity: 2,
+        info: [
+            {id: 1, busy: false},
+            {id: 2, busy: true}
+        ]
+    }
+}
+
 console.log(student.name)
 console.log(student.age)
 console.log(student["Моя Фамилия"])
 console.log(student.address.city.title)
 console.log(student.technologies[3].title)
+
+console.log(tomato.places.info[1].busy)
