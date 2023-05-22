3bf0cb2c2a66e684cb324353c5146b5f7e4f1baf
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-05-22 22:45:21 +0300

    Updated file

--- JS-TS/src/2023/03-Function-2023/2023.03.ts
+++ JS-TS/src/2023/03-Function-2023/2023.03.ts
@@ -1,4 +1,14 @@
-debugger
-export const sum = (a:number, b: number) => {
-    return a+b
+import {StudentType} from "../02-Object-2023/2023.02";
+
+export const sum = (a: number, b: number) => {
+    return a + b
+}
+
+//const res = sum(sum(1, 2), sum(2, 4))
+
+export const addSkills = (st: StudentType, title: string) => {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: title
+    })
 }
