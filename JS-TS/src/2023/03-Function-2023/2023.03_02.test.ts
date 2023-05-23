4e6d889ce9ea3376d511ed8e7af497a478631d4d
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-05-23 22:37:51 +0300

    Added file

---
+++ JS-TS/src/2023/03-Function-2023/2023.03_02.test.ts
@@ -0,0 +1,74 @@
+import {CityType} from "../02-Object-2023/2023.02_02";
+
+
+let city: CityType
+
+beforeEach(() => {
+    city = {
+        title: "Moscow",
+        houses: [
+            {
+                buildedAt: 2013, repaired: false,
+                address: {number: 1, street: {title: "Lipchanskogo"}}
+            },
+            {
+                buildedAt: 1980, repaired: false,
+                address: {number: 8, street: {title: "Sormovskaya"}}
+            },
+            {
+                buildedAt: 1969, repaired: false,
+                address: {number: 4, street: {title: "Uralskaya"}}
+            }
+            ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL", budget: 200000, staffCount: 1000,
+                address: {
+                    street: {
+                        title: "Nedorubova"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION", budget: 500000, staffCount: 1000,
+                address: {
+                    street: {
+                        title: "Dmitrievsky"
+                    }
+                }
+            }],
+        citizensNumber: 12000000
+    }
+})
+// 01. Создайте в отдельном файле функцию, чтобы тесты прошли
+test.skip('Budget should be for changed fot HOSPITAL', () => {
+    addMoneyToBudget(city.governmentBuildings[0], 100000);
+    expect(city.governmentBuildings[0].budget).toBe(300000);
+});
+// 02. Тесты должны пройти
+test.skip('Budget should be changed for FIRE-STATION', () => {
+    addMoneyToBudget(city.governmentBuildings[1], -100000);
+    expect(city.governmentBuildings[1].budget).toBe(400000);
+});
+
+
+// 03. Создайте в том же файле ещё одну функцию, чтобы тесты прошли
+test.skip('House should be repaired', () => {
+    repairHouse(city.houses[1]);
+    expect(city.houses[1].repaired).toBeTruthy();
+});
+
+
+// 04. Создайте в том же файле еще одну функцию, чтобы тесты прошли
+test.skip('Staff should be reduced', () => {
+    toFireStaff(city.governmentBuildings[0], 20);
+
+    expect(city.governmentBuildings[0].staffCount).toBe(180);
+});
+// 05. Создайте в том же файле еще одну функцию, чтобы тесты прошли
+test.skip('Staff should be increased', () => {
+    toHireStaff(city.governmentBuildings[0], 20);
+
+    expect(city.governmentBuildings[0].staffCount).toBe(220);
+});
+})
